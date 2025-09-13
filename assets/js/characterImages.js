2d1f65cb6a6ae31662be5814e8b2e6b5437840d1
Aleksandr Nekrasov <aleksandr.nekrasov@agoda.com>
2025-09-13 16:08:26 +0700

    rename images

--- assets/js/characterImages.js
+++ assets/js/characterImages.js
@@ -3,44 +3,44 @@
 // Contains image URLs for different characters' cards and questions.
 const characterImages = {
     crab: {
-        getShoreCardBack: function() { return 'assets/cards/crab/back_shore.png'; },
-        getShoreCardFront: function() { return 'assets/cards/crab/front_shore.png'; },
-        getShallowCardBack: function() { return 'assets/cards/crab/back_shallow.png'; },
-        getShallowCardFront: function() { return 'assets/cards/crab/front_shallow.png'; },
-        getDepthCardBack: function() { return 'assets/cards/crab/back_depth.png'; },
-        getDepthCardFront: function() { return 'assets/cards/crab/front_depth.png'; }
+        getShoreCardBack: function() { return 'assets/cards/fugitive/back_shore.png'; },
+        getShoreCardFront: function() { return 'assets/cards/fugitive/front_shore.png'; },
+        getShallowCardBack: function() { return 'assets/cards/fugitive/back_shallow.png'; },
+        getShallowCardFront: function() { return 'assets/cards/fugitive/front_shallow.png'; },
+        getDepthCardBack: function() { return 'assets/cards/fugitive/back_depth.png'; },
+        getDepthCardFront: function() { return 'assets/cards/fugitive/front_depth.png'; }
     },
     dolphin: {
-        getShoreCardBack: function() { return 'assets/cards/dolphin/back_shore.png'; },
-        getShoreCardFront: function() { return 'assets/cards/dolphin/front_shore.png'; },
-        getShallowCardBack: function() { return 'assets/cards/dolphin/back_shallow.png'; },
-        getShallowCardFront: function() { return 'assets/cards/dolphin/front_shallow.png'; },
-        getDepthCardBack: function() { return 'assets/cards/dolphin/back_depth.png'; },
-        getDepthCardFront: function() { return 'assets/cards/dolphin/front_depth.png'; }
+        getShoreCardBack: function() { return 'assets/cards/clown/back_shore.png'; },
+        getShoreCardFront: function() { return 'assets/cards/clown/front_shore.png'; },
+        getShallowCardBack: function() { return 'assets/cards/clown/back_shallow.png'; },
+        getShallowCardFront: function() { return 'assets/cards/clown/front_shallow.png'; },
+        getDepthCardBack: function() { return 'assets/cards/clown/back_depth.png'; },
+        getDepthCardFront: function() { return 'assets/cards/clown/front_depth.png'; }
     },
     pufferfish: {
-        getShoreCardBack: function() { return 'assets/cards/pufferfish/back_shore.png'; },
-        getShoreCardFront: function() { return 'assets/cards/pufferfish/front_shore.png'; },
-        getShallowCardBack: function() { return 'assets/cards/pufferfish/back_shallow.png'; },
-        getShallowCardFront: function() { return 'assets/cards/pufferfish/front_shallow.png'; },
-        getDepthCardBack: function() { return 'assets/cards/pufferfish/back_depth.png'; },
-        getDepthCardFront: function() { return 'assets/cards/pufferfish/front_depth.png'; }
+        getShoreCardBack: function() { return 'assets/cards/panicked-speaker/back_shore.png'; },
+        getShoreCardFront: function() { return 'assets/cards/panicked-speaker/front_shore.png'; },
+        getShallowCardBack: function() { return 'assets/cards/panicked-speaker/back_shallow.png'; },
+        getShallowCardFront: function() { return 'assets/cards/panicked-speaker/front_shallow.png'; },
+        getDepthCardBack: function() { return 'assets/cards/panicked-speaker/back_depth.png'; },
+        getDepthCardFront: function() { return 'assets/cards/panicked-speaker/front_depth.png'; }
     },
     eel: {
-        getShoreCardBack: function() { return 'assets/cards/eel/back_shore.png'; },
-        getShoreCardFront: function() { return 'assets/cards/eel/front_shore.png'; },
-        getShallowCardBack: function() { return 'assets/cards/eel/back_shallow.png'; },
-        getShallowCardFront: function() { return 'assets/cards/eel/front_shallow.png'; },
-        getDepthCardBack: function() { return 'assets/cards/eel/back_depth.png'; },
-        getDepthCardFront: function() { return 'assets/cards/eel/front_depth.png'; }
+        getShoreCardBack: function() { return 'assets/cards/writer/back_shore.png'; },
+        getShoreCardFront: function() { return 'assets/cards/writer/front_shore.png'; },
+        getShallowCardBack: function() { return 'assets/cards/writer/back_shallow.png'; },
+        getShallowCardFront: function() { return 'assets/cards/writer/front_shallow.png'; },
+        getDepthCardBack: function() { return 'assets/cards/writer/back_depth.png'; },
+        getDepthCardFront: function() { return 'assets/cards/writer/front_depth.png'; }
     },
     flatfish: {
-        getShoreCardBack: function() { return 'assets/cards/flatfish/back_shore.png'; },
-        getShoreCardFront: function() { return 'assets/cards/flatfish/front_shore.png'; },
-        getShallowCardBack: function() { return 'assets/cards/flatfish/back_shallow.png'; },
-        getShallowCardFront: function() { return 'assets/cards/flatfish/front_shallow.png'; },
-        getDepthCardBack: function() { return 'assets/cards/flatfish/back_depth.png'; },
-        getDepthCardFront: function() { return 'assets/cards/flatfish/front_depth.png'; }
+        getShoreCardBack: function() { return 'assets/cards/invisible/back_shore.png'; },
+        getShoreCardFront: function() { return 'assets/cards/invisible/front_shore.png'; },
+        getShallowCardBack: function() { return 'assets/cards/invisible/back_shallow.png'; },
+        getShallowCardFront: function() { return 'assets/cards/invisible/front_shallow.png'; },
+        getDepthCardBack: function() { return 'assets/cards/invisible/back_depth.png'; },
+        getDepthCardFront: function() { return 'assets/cards/invisible/front_depth.png'; }
     }
 };
 
